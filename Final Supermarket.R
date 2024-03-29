71364bdb791a8fc27b4aeaa46db6d6e81ec97df4
FrancoFiala <114265971+FrancoFiala@users.noreply.github.com>
2024-03-29 12:52:21 -0300

    Final Commit Google

---
+++ Final Supermarket.R
@@ -0,0 +1,4 @@
+read.csv("https://raw.githubusercontent.com/FrancoFiala/GoogleFinalProject/main/supermarket_sales.csv")
+read.csv('https://raw.githubusercontent.com/FrancoFiala/GoogleFinalProject/main/supermarket_sales.csv')
+data <- read.csv('https://raw.githubusercontent.com/FrancoFiala/GoogleFinalProject/main/supermarket_sales.csv')
+
